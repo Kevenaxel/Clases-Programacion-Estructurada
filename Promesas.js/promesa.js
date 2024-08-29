70fefee7ff60d94aefeeecd8558afee3914ce19f
Kevenaxel <hectorsastre476@gmail.com>
2024-08-28 20:55:07 -0600

    new

---
+++ Promesas.js/promesa.js
@@ -0,0 +1,16 @@
+let x=10
+
+const promesa = new Promise((resolve, reject)=>{
+    if(x==11){
+        resolve('La variable es igual a 10')
+    }else{
+        reject('La variable no es igual a 10')
+    }
+})
+
+promesa.then(res =>{
+    console.log('Mensaje:' +res)
+})
+.catch(error=>{
+    console.log('Error:'+ error)
+})
