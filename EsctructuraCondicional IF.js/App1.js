1191792b09463b458b9f437abfad91da2b154ed2
Kevenaxel <hectorsastre476@gmail.com>
2024-09-16 20:03:08 -0600

    new

---
+++ EsctructuraCondicional IF.js/App1.js
@@ -0,0 +1,43 @@
+// if (condicion){
+    // Codigo a ejecutar
+// }
+
+// let num = 8
+
+// if (num > 5){
+    //console.log('El numero es mayor que 5')
+// }
+
+// if (condicion){
+    // Codigo si la condicion es verdadera
+// }else{
+    // La condicion es falsa 
+// }
+
+// let numero = 3
+// if (numero > 5){
+    //console.log('EL numero es mayor que 5')
+// }else{
+    //console.log('El numero es menor o igual que 5')
+// }
+
+// Multiple
+
+// if (condicion){
+    //Codigo si la condicion es verdadera
+// }else if (condicion){
+    //Codigo si la condicion es verdadera
+// }else if(condicion){
+    //Codigo si la condicion es verdadera
+// }else{
+    //Codnicion si las opciones anteriores no han sido verdaderas
+// }
+
+let numero = 10
+if (numero > 10){
+    console.log('El numero es mayor que 10')
+}else if(numero ===10){
+    console.log('El numero es igual a 10')
+}else {
+    console.log('El numero es menor que 10')
+}
---
+++ EstructuraSwith.js/App1.js
@@ -0,0 +1,29 @@
+let categoria ="B"
+switch (categoria){
+    case "A":
+        console.log("Precio: $50.00")
+        break
+    case "B":
+        console.log("Precio: $30.00")
+        break
+    case "C":
+        console.log("Precio: 20.00")
+        break
+    default:
+        console.log("Categoria no valida")
+}
+
+let opcion = 3
+switch (opcion){
+    case 1:
+        console.log("Has seleccionado Opcion 1: Ver Perfil")
+        break
+    case 2:
+        console.log("Has seleccionado Opcion 2: Editar Perfil")
+        break
+    case 3:
+        console.log("Has seleccionado Opcion 3: Cerrar Seccion")
+        break
+    default:
+        console.log("Opcion no valida")
+}
