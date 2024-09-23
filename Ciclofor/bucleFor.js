5e906da4d25af4ae3917fe5d809d23c87489f1c6
Kevenaxel <hectorsastre476@gmail.com>
2024-09-23 10:29:22 -0600

    new

---
+++ Ciclofor/bucleFor.js
@@ -0,0 +1,5 @@
+// Ejemplo basico
+
+for(let i=0; i<=5; i++){
+    console.log("Interacion Numero: "+ i);
+} 
---
+++ Ciclofor/bucleFor2.js
@@ -0,0 +1,4 @@
+// Ejercicio al infinito sin dar respuesta
+for (let i=0;i<5;){
+    console.log("Interacion numeros:");
+}
