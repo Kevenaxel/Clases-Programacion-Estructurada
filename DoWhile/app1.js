88a3b0d3139e773ddfd059b569445a600de41cb6
Kevenaxel <hectorsastre476@gmail.com>
2024-10-02 14:51:49 -0600

    new

---
+++ DoWhile/app1.js
@@ -0,0 +1,13 @@
+//do{
+    
+//}while(condicion);
+
+let numero =20;
+
+while(numero<10){
+    console.log(numero)
+}
+
+do{
+    console.log(numero)
+}while(numero <10);
---
+++ DoWhile/app3.js
@@ -0,0 +1,9 @@
+let suma = 0;
+let numero = 1;
+
+do{
+    suma+= numero;
+    numero++
+}while(numero <=100);
+
+console.log('La suma de los numeros del 1 al 100 es:' + suma)
